876bb98ace5f5d947f54113955c01748b4a5be88
Passuum2 <Darityreesemyles@gmail.com>
2025-08-19 22:21:51 -0500

    Addes sorting

---
+++ Sorting/CPCS_1110_Ch14_E04_Darity.java
@@ -0,0 +1,16 @@
+/* CPCS_1110_Ch14_E04_Darity.java - Solution to Hortsmann Chapter 14, Exercise 04
+ * Myles Darity-Reese
+ * 13-Oct-23
+ * Problem Statement:
+ * Modify the merge sort algorithm to sort an array of strings
+ * in lexicographic order.
+ */
+
+public class CPCS_1110_Ch14_E04_Darity {
+    //CPCS_1110_Ch14_E04_Darity.java - Solution to Hortsmann Chapter 14, Exercise 04
+    public static void main(String[] args) {
+        //main(): StringSort.main(args);
+        //Implementation: Calls to MergeSort Class
+        MergeSort.main(args);
+    }
+}
